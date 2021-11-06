a43b2ed497c0e97076d2a3dcae083e3b410a26c0
Evgeniy <volrado@bk.ru>
2021-11-07 02:05:36 +0300

    add modal adaptive, add masked phone, add validation forms

--- src/js/script.js
+++ src/js/script.js
@@ -47,9 +47,6 @@
         $('.modal__close').on('click', function() {
             $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
         });  
-        // $('.button_mini').on('click', function() {
-        //     $('.overlay, #order').fadeIn('slow');
-        // });
 
         $('.button_mini').each(function(i) {
             $(this).on('click', function() {
@@ -57,6 +54,39 @@
                $('.overlay, #order').fadeIn('slow');
             });
         });
+
+        function valideForms(form) {
+            $(form).validate({
+                rules: {
+                    name: {
+                        required: true,
+                        minlength: 2
+                    },
+                    phone: "required",
+                    email: {
+                        required: true,
+                        email: true
+                    }
+                },
+                messages: {
+                    name: {
+                        required: "Пожалуйста, введите свое имя",
+                        minlength: jQuery.validator.format("Введите {0} символа")
+                    },
+                    phone: 'Пожалуйста, введите свой номер телефона',
+                    email: {
+                        required: "Пожалуйста, введите свою почту",
+                        email: "Неправильно введен адрес почты"
+                    }
+                }
+            });
+        };
+
+        valideForms('#consultation-form');
+        valideForms('#consultation form');
+        valideForms('#order form');
+
+        $('input[name=phone]').mask("+7 (999) 999-99-99");
     });
 })(jQuery);
 
